5e1c8c3e941e26a4154503c49d39ff3bd01403d3
AmmyK <kojikeisisu@gmail.com>
2022-09-15 22:20:14 +0900

    Initial Commit

---
+++ drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drawing
+//
+//  Created by amamiya on 2022/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by amamiya on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
